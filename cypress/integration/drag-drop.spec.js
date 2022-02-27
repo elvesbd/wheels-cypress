e55802bb3b0ac74e410fc891b3ebd1ce19cef76b
elvesbd <elvesbd@yahoo.com>
2022-02-27 11:09:24 -0300

    refactoring test drag-drop with using hooks

--- cypress/integration/drag-drop.spec.js
+++ cypress/integration/drag-drop.spec.js
@@ -1,17 +1,19 @@
-it('should be drag cypress icon to node box', () => {
-  cy.visit('https://training-wheels-qaninja.herokuapp.com/drag_and_drop')
+describe('Drag and Drop', () => {
+  before(() => {
+    cy.visit('https://training-wheels-qaninja.herokuapp.com/drag_and_drop')
+  })
 
-  const dataTransfer = new DataTransfer();
-
-  cy.get('img[alt="Cypress"]').trigger('dragstart', {dataTransfer})
-  cy.get('.nodejs figure[draggable=true]').trigger('drop', {dataTransfer})
-});
-
-it('should be drag robot icon to python box', () => {
-  cy.visit('https://training-wheels-qaninja.herokuapp.com/drag_and_drop')
-
-  const dataTransfer = new DataTransfer();
-
-  cy.get('img[alt="Robot Framework"]').trigger('dragstart', {dataTransfer})
-  cy.get('.python figure[draggable=true]').trigger('drop', {dataTransfer})
+  it('should be drag cypress icon to node box', () => {
+    const dataTransfer = new DataTransfer();
+  
+    cy.get('img[alt="Cypress"]').trigger('dragstart', {dataTransfer})
+    cy.get('.nodejs figure[draggable=true]').trigger('drop', {dataTransfer})
+  });
+  
+  it('should be drag robot icon to python box', () => {  
+    const dataTransfer = new DataTransfer();
+  
+    cy.get('img[alt="Robot Framework"]').trigger('dragstart', {dataTransfer})
+    cy.get('.python figure[draggable=true]').trigger('drop', {dataTransfer})
+  });
 });
