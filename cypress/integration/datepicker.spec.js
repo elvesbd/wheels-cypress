b9805b24b123dabe32cfb16cf916c440bc6ae928
elvesbd <elvesbd@yahoo.com>
2022-03-06 17:26:10 -0300

    selecting date in datepicker

---
+++ cypress/integration/datepicker.spec.js
@@ -0,0 +1,19 @@
+it('should be select birth data', () => {
+  cy.visit('/datepicker')
+
+  const date = {
+    month: 'fev',
+    year: '1983',
+    day: '13'
+  }
+
+  cy.get('.datetimepicker-dummy-input').click()
+
+  cy.get('.datepicker-nav-month').click()
+  cy.contains('.datepicker-month', date.month).click()
+
+  cy.get('.datepicker-nav-year').click()
+  cy.contains('.datepicker-year span', date.year).click()
+
+  cy.contains('button[class=date-item]', date.day).click()
+});
