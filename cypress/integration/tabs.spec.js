416a2f42c2d830089abb1734024cf37d97ded60a
elvesbd <elvesbd@yahoo.com>
2022-03-20 16:56:45 -0300

    create tests in new tab

---
+++ cypress/integration/tabs.spec.js
@@ -0,0 +1,11 @@
+it('new tab', () => {
+  cy.visit('/new_tab')
+
+  // cy.contains('a', 'Clique aqui').click()
+  cy.get('a[href="https://www.google.com.br"]').should('have.attr', 'target', '_blank')
+
+  cy.contains('a', 'Clique aqui').invoke('removeAttr', 'target')
+  cy.contains('a', 'Clique aqui').click()
+
+  cy.get('input[name=q]').type('Cypress{enter}')
+});
