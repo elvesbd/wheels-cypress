70aa130255cbb1602987cbbd85bf902b06bdaad3
elvesbd <elvesbd@yahoo.com>
2022-03-20 13:01:18 -0300

    create test event pop up

--- cypress/integration/alerts.spec.js
+++ cypress/integration/alerts.spec.js
@@ -6,3 +6,19 @@ it('should display an alert message', () => {
     expect(result).to.equal('Isto é uma mensagem de alerta')
   })
 });
+
+it('should confirm the request', () => {
+  cy.visit('/javascript_alerts')
+
+  cy.contains('button', 'Confirma').click()
+  cy.on('window:confirm', () => true)
+  cy.get('#result').should('have.text', 'Mensagem confirmada')
+});
+
+it('should canceled the request', () => {
+  cy.visit('/javascript_alerts')
+
+  cy.contains('button', 'Confirma').click()
+  cy.on('window:confirm', () => false)
+  cy.get('#result').should('have.text', 'Mensagem não confirmada')
+});
